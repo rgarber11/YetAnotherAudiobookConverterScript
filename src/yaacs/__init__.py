029dfb9a1ce29c0b3e47a57012b82985201e1d26
Richard Garber <rg.1029384756@gmail.com>
2025-05-04 22:06:27 -0400

    Added top-level logging info to __init__

--- src/yaacs/__init__.py
+++ src/yaacs/__init__.py
@@ -1,4 +1,21 @@
+import logging.config
+
 from yaacs.cli import main
 
+logging.config.dictConfig(
+    {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {"simple": {"format": "%(message)s"}},
+        "handlers": {
+            "stdout": {
+                "class": "logging.StreamHandler",
+                "formatter": "simple",
+                "stream": "ext://sys.stdout",
+            }
+        },
+        "loggers": {"root": {"level": "WARNING", "handlers": ["stdout"]}},
+    }
+)
 if __name__ == "__main__":
     main()
