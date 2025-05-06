91a7334fc6fb2c11b02ac37049e9f9c1e42d618b
Richard Garber <rg.1029384756@gmail.com>
2025-05-06 11:07:45 -0400

    Change build to auto-create cue parser

--- setup.py
+++ setup.py
@@ -1,3 +1,46 @@
-from setuptools import setup
+from typing import override
 
-setup()
+import setuptools.command.build
+from setuptools import Command, setup
+
+
+class build_parser(Command):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.build_lib = None
+        self.editable_mode = False
+
+    @override
+    def run(self):
+        from lark import Lark
+        from lark.tools.standalone import gen_standalone
+
+        print(self.build_lib)
+        with (
+            open("src/yaacs/cue/cue.lark", "r") as grammarFile,
+            open(f"{self.build_lib}/yaacs/cue/cue.py", "w") as outfile,
+        ):
+            lalrParser = Lark(grammarFile, parser="lalr")
+            gen_standalone(lalrParser, out=outfile)
+
+    @override
+    def initialize_options(self) -> None:
+        self.build_lib = None
+        self.editable_mode = False
+
+    @override
+    def finalize_options(self):
+        self.set_undefined_options("build_py", ("build_lib", "build_lib"))
+
+    def get_outputs(self) -> list[str]:
+        return [f"{self.build_lib}/yaacs/cue/cue.py"]
+
+    def get_output_mapping(self) -> dict[str, str]:
+        return {f"{self.build_lib}/yaacs/cue/cue.py": "src/yaacs/cue/cue.lark"}
+
+    def get_source_files(self):
+        return ["src/yaacs/cue/cue.lark"]
+
+
+setuptools.command.build.build.sub_commands.append(("build_lark", None))
+setup(cmdclass={"build_lark": build_parser})
